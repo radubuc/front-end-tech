963e912eb72bc5cf5c675e356782b8743f862b5a
Renee Dubuc <rdubuc@GYKantler.com>
2022-01-09 12:19:29 -0500

    Finished js for all fields-date, mood, topic, emotions, thoughts, distortions, feelings and delete button

--- week-4/hw/app.js
+++ week-4/hw/app.js
@@ -8,41 +8,66 @@ document.getElementById('add').addEventListener('click', () => {
 
     row.insertCell(0).innerHTML = `${createdDate.getFullYear()}-${createdDate.getMonth() + 1}-${createdDate.getDate()}`; //Sets value of 1st cell, under Date header (yyyy-mm-dd because it's easier)
 
-    //Mood showing as undefined. How do I get this to see the value of the radio buttons? I tried adding id="mood" everywhere I could and also setting value="terrible" on the mood input tag
-    // row.insertCell(1).innerHTML = document.getElementById('mood').value; //Sets value of 2nd cell in row, under Mood header
+    //Mood attempt 6--Success!
+    let moodRadio = document.getElementsByName('moodRadio');
+    for (let i = 0; i < moodRadio.length; i++) {
+        if (moodRadio[i].checked) {
+            row.insertCell(1).innerHTML = moodRadio[i].value;
+
+        }
+    }
     
-    //Mood attempt 2--Used moodRadioInline1 from mood input tag. Result is "on", which probably means it see the first radio button is selected, but not what I'm looking for.
-    // row.insertCell(1).innerHTML = document.getElementById('moodRadioInline1').value; //Sets value of 2nd cell in row, under Mood header
+    //Topic attempt 4--Success!
+    let topicCheckbox = document.getElementsByName('topicCheckbox');
+    let topicCheckboxValue = "";
+    for (let i = 0; i < topicCheckbox.length; i++) {
+        if (topicCheckbox[i].checked) {
+            topicCheckboxValue+=topicCheckbox[i].value + ", ";
 
-    //Mood attempt 3--Used .innerHTML instead of .value. Result is "Mood" with each of the 5 radio buttons and their text
-    // row.insertCell(1).innerHTML = document.getElementById('mood').innerHTML; //Sets value of 2nd cell in row, under Mood header
+        }
+    }
+    let tcv = topicCheckboxValue.slice(0, topicCheckboxValue.length - 2);
+    row.insertCell(2).innerHTML = tcv;
 
-    //Mood attempt 4--Used W3Schools https://www.w3schools.com/jsref/prop_radio_value.asp Result is "Terrible" no matter which mood I chose. This is because moodRadioInline1 corresponds to "Terrible", so how can I look at just the mood I chose?
-    var m = document.getElementById("moodRadioInline1").value;
-    row.insertCell(1).innerHTML = m; //Sets value of 2nd cell in row, under Mood header
+    //Emotions attempt 2--Success!
+    let emotionsCheckbox = document.getElementsByName('emotionsCheckbox');
+    let emotionsCheckboxValue = "";
+    for (let i = 0; i < emotionsCheckbox.length; i++) {
+        if (emotionsCheckbox[i].checked) {
+            emotionsCheckboxValue+=emotionsCheckbox[i].value + ", ";
 
-    //Mood attempt 5--Based on attempt 4. Removed number at end of each of the 5 moods here and in index. Result is all mood radio buttons are disabled
-    // var m = document.getElementById("moodRadioInline").value;
-    // row.insertCell(1).innerHTML = m; //Sets value of 2nd cell in row, under Mood header
-    
-    //Topic attempt 1--Result is "undefined", despite each topic having a corresponding value attribute
-    // row.insertCell(2).innerHTML = document.getElementById('topic').value; //Sets value of 3rd cell, under Topic header
-    
-    //Topic attempt 2--Used Mood attempt 4 as a template. Result is Topic = Family no matter what option I choose because topicInlineCheckbox corresponds to "Family". Since this is a checkbox, how can I pull multiple values?
-    // var t = document.getElementById("topicInlineCheckbox1").value;
-    // row.insertCell(2).innerHTML = t; //Sets value of 3rd cell, under Topic header
-    
-    //Topic attempt 3--Used SO https://stackoverflow.com/questions/15839169/how-to-get-value-of-selected-radio-button  Result is "false". Must be a Boolean checking if "Family" is selected, which it wasn't. I chose "Friends". Do I need an if statement?
-    var t = document.getElementById("topicInlineCheckbox1").checked;
-    row.insertCell(2).innerHTML = t; //Sets value of 3rd cell, under Topic header
+        }
+    }
+    let ecv = emotionsCheckboxValue.slice(0, emotionsCheckboxValue.length - 2);
+    row.insertCell(3).innerHTML = ecv;
 
-    row.insertCell(3).innerHTML = document.getElementById('emotions').value; //Sets value of 4th cell, under Emotions header
-
+    //Thoughts attempt 1--Success!
     row.insertCell(4).innerHTML = document.getElementById('thoughts').value; //Sets value of 4th cell, under Thoughts header
 
-    row.insertCell(5).innerHTML = document.getElementById('distortions').value; //Sets value of 4th cell, under Distortions header
+    //Distortions attempt 3--Success!
+    let distortionsCheckbox = document.getElementsByName('distortionsCheckbox');
+    let distortionsCheckboxValue = "";
+    for (let i = 0; i < distortionsCheckbox.length; i++) {
+        if (distortionsCheckbox[i].checked) {
+            distortionsCheckboxValue+=distortionsCheckbox[i].value + ", ";
 
-    row.insertCell(6).innerHTML = document.getElementById('feelings').value; //Sets value of 4th cell, under Feelings header
+        }
+    }
+    let dcv = distortionsCheckboxValue.slice(0, distortionsCheckboxValue.length - 2);
+    row.insertCell(5).innerHTML = dcv;
+    
+    //Feelings attempt 2--
+    let feelingsRadio = document.getElementsByName('feelingsRadio');
+    for (let i = 0; i < feelingsRadio.length; i++) {
+        if (feelingsRadio[i].checked) {
+            row.insertCell(6).innerHTML = feelingsRadio[i].value;
+
+        }
+    }
+
+
+
+
 
     let actions = row.insertCell(7); //Variable created for Delete button
     actions.appendChild(createDeleteButton(id++)); //Beginning of delete button. This increments ID
@@ -64,3 +89,51 @@ function createDeleteButton(id){
     };
     return btn; //Returns button at end
 }
+
+
+
+
+
+
+// Scratch
+//Mood showing as undefined. How do I get this to see the value of the radio buttons? I tried adding id="mood" everywhere I could and also setting value="terrible" on the mood input tag
+    // row.insertCell(1).innerHTML = document.getElementById('mood').value; //Sets value of 2nd cell in row, under Mood header
+    
+    //Mood attempt 2--Used moodRadioInline1 from mood input tag. Result is "on", which probably means it see the first radio button is selected, but not what I'm looking for.
+    // row.insertCell(1).innerHTML = document.getElementById('moodRadioInline1').value; //Sets value of 2nd cell in row, under Mood header
+
+    //Mood attempt 3--Used .innerHTML instead of .value. Result is "Mood" with each of the 5 radio buttons and their text
+    // row.insertCell(1).innerHTML = document.getElementById('mood').innerHTML; //Sets value of 2nd cell in row, under Mood header
+
+    //Mood attempt 4--Used W3Schools https://www.w3schools.com/jsref/prop_radio_value.asp Result is "Terrible" no matter which mood I chose. This is because moodRadioInline1 corresponds to "Terrible", so how can I look at just the mood I chose?
+    // var m = document.getElementById("moodRadioInline1").value;
+    // row.insertCell(1).innerHTML = m; //Sets value of 2nd cell in row, under Mood header
+
+    //Mood attempt 5--Based on attempt 4. Removed number at end of each of the 5 moods here and in index. Result is all mood radio buttons are disabled
+    // var m = document.getElementById("moodRadioInline").value;
+    // row.insertCell(1).innerHTML = m; //Sets value of 2nd cell in row, under Mood header
+
+//Topic attempt 1--Result is "undefined", despite each topic having a corresponding value attribute
+    // row.insertCell(2).innerHTML = document.getElementById('topic').value; //Sets value of 3rd cell, under Topic header
+    
+    //Topic attempt 2--Used Mood attempt 4 as a template. Result is Topic = Family no matter what option I choose because topicInlineCheckbox corresponds to "Family". Since this is a checkbox, how can I pull multiple values?
+    // var t = document.getElementById("topicInlineCheckbox1").value;
+    // row.insertCell(2).innerHTML = t; //Sets value of 3rd cell, under Topic header
+    
+    //Topic attempt 3--Used SO https://stackoverflow.com/questions/15839169/how-to-get-value-of-selected-radio-button  Result is "false". Must be a Boolean checking if "Family" is selected, which it wasn't. I chose "Friends". Do I need an if statement?
+    // var t = document.getElementById("topicInlineCheckbox1").checked;
+    // row.insertCell(2).innerHTML = t; //Sets value of 3rd cell, under Topic header
+
+    //Emotions attempt 1
+    // row.insertCell(3).innerHTML = document.getElementById('emotions').value; //Sets value of 4th cell, under Emotions header
+
+    //Distortions attempt 1
+    // row.insertCell(5).innerHTML = document.getElementById('distortions').value; //Sets value of 4th cell, under Distortions header
+
+    //Distortions attempt 2--
+    // var d = document.getElementById("distortionsCheckbox1").value;
+    // row.insertCell(5).innerHTML = d; //Sets value of 5th cell, under Distortions header
+    // //Copy from emotions radio code
+
+    //Feelings attempt 1
+    // row.insertCell(6).innerHTML = document.getElementById('feelings').value; //Sets value of 4th cell, under Feelings header
