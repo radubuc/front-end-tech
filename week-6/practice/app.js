a6b79484435b8d7ed734809222484db2090fe00e
Renee Dubuc <rdubuc@GYKantler.com>
2022-01-29 14:08:06 -0500

    Added FUll CRUD App Part 2 video notes up to 12 min

--- week-6/practice/app.js
+++ week-6/practice/app.js
@@ -1,4 +1,4 @@
-//FULL CRUD APP PART 1 VIDEO NOTES
+//FULL CRUD APP PART 1 & 2 VIDEO NOTES
 class House {
     constructor(name) {
         this.name = name;
@@ -51,14 +51,45 @@ class HouseService { //CRUD operations
 }
 
 class DOMManager {
-    static houses;
+    static houses; //Allows us to find house that matches id that's passed into
 
     static getAllHouses() { //method doesn't exist yet - Top Down dev
         HouseService.getAllHouses().then(houses => this.render(houses)); //Takes a callback. Sets up getting all houses and renders to DOM. Render method not created yet
-
     }
 
-    static render(houses){ //Takes list of houses and renders houses to DOM
+    static createHouse(name) {
+        HouseService.createHouse(new House(name))
+        .then(() => { //Handles promise that comes back
+            return HouseService.getAllHouses(); //Notice a pattern?
+        })
+        .then((houses) => this.render(houses)); //Create house, then render all houses
+    }
+
+    static deleteHouse(id) {
+        HouseService.deleteHouse(id)
+            .then(() => {
+                return HouseService.getAllHouses(); //When we delete a house successfully, we want to get all houses again
+            })
+            .then((houses) => this.render(houses)); //Then we want to render houses again
+    }
+
+    static addRoom(id) {
+        for (let house of this.houses) { //Loop through array to find house
+            if (house._id == id) { //If current house id matches house id we passed in...
+                house.rooms.push(new Room($(`#${house._id}-room-name`).val(), $(`#${house._id}-room-area`).val())); //Push a new room (with name and area) to rooms array. Breakdown of symbols: $() jQuery, `${}` template literal, # find by id
+                HouseService.updateHouse(house) //Take house whose new room I just added to rooms array and send update request to API to save new data
+                    .then(() => {
+                        console.log("1st .then in addRoom function");
+                        return HouseService.getAllHouses(); //Gets all houses
+                    })
+                    
+                    .then((houses) => this.render(houses)); //Rerenders houses after update
+                    console.log("2nd .then in addRoom function")
+            }
+        }
+    }
+
+    static render(houses) { //Takes list of houses and renders houses to DOM
         this.houses = houses;
         $('#app').empty(); //Clears everytime we render, so we can later rerender
         for (let house of houses) { //Goes over all houses and renders
@@ -67,39 +98,55 @@ class DOMManager {
                     <div class="card-header">
                         <h2>${house.name}</h2>
                         <button class="btn btn-danger" onclick="DOMManager.deleteHouse('${house._id}')">Delete House</button>
-                    </div>
+                    </div> 
                     <div class="card-body>
                         <div class="card">
                             <div class="row">
-                                <div class="col-sm"
+                                <div class="col-sm">
                                     <input type="text" id="${house._id}-room-name" class="form-control" placeholder="Room Name">
                                 </div>
-                                <div class="col-sm"
+                                <div class="col-sm">
                                     <input type="text" id="${house._id}-room-area" class="form-control" placeholder="Room Area">
                                 </div>
-                            </div>
-                            <button id="${house._id}-new-room" onclick="DOMManager.addRoom('${house._id}')" class="btn btn-primary form-control">Add</button>
+                            </div><br />
+                            <button id="${house._id}-new-room" onclick="DOMManager.addRoom('${house._id}')" class="btn btn-primary form-control">Add Room</button>
                         </div>
                     </div>
                 </div><br />`
 
                 //Line 75: When we create new room, method will know which house id to take and add room to 
-            ); // Stopped working at timestamp 22 min in video, before adding Delete button, card body, and Add button divs above
+            ); // Stopped working at timestamp 22 min in video, before adding Delete button, card body, and Add button divs above. Update: Works suddenly \_(o_O)_/
+
             for (let room of house.rooms) { //House refers to current iterations' house
                 $(`#${house._id}`).find('card-body').append(
                     `<p>
                         <span id="name-${room._id}"><strong>Name: </strong> ${room.name}</span>
                         <span id="area-${room._id}"><strong>Area: </strong> ${room.area}</span>
                         <button class="btn btn-danger" onclick="DOMManager.deleteRoom('${house._id}', '${room._id}')">Delete Room</button>`
-                )
+                );
             }
         }
     }
 }
 
+$('#create-new-house').click(() => {
+    DOMManager.createHouse($('#new-house-name').val());
+    $('#new-house-name').val('');
+});
+
 DOMManager.getAllHouses();
 
 // Stopped working at timestamp 22 min in video, before adding nested rooms for loop above 
+//Update: After restarting it up 2 days later, it works as intended without any changes! Ok
+
+//CRUD PART 1 SUMMARY
+//HTML - 2 DIVS
+//JS - HOUSE, ROOM, HOUSESERVICE, DOMMANAGER CLASSES CREATED
+//JS - HOUSESERVICE: GETALLHOUSES, GETHOUSE, CREATEHOUSE, UPDATEHOUSE, DELETEHOUSE
+//JS - DOMMANGAGER: GETALLHOUSES, RENDER, ROOM/HOUSE FOR LOOP
+
+
+//CRUD PART 2 SUMMARY
 
 
 
